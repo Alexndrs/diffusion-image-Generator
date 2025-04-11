0668eacc192aa58dc7f31df5c32f38173131bdcb
juliensegonne <julien.segonne@ensta-paris.fr>
2025-04-11 16:05:34 -0400

    Modele fini

--- model/unet.py
+++ model/unet.py
@@ -156,3 +156,57 @@ class Downsample(nn.Module):        #Scale down the feature map by 21​×
         
     
 class UNet(nn.Module):
+    def __init__(self, image_channels: int = 3, n_channels: int = 64, ch_mults: Union[Tuple[int, ...], List[int]] = (1, 2, 2, 4),
+                 is_attn: Union[Tuple[bool, ...], List[bool]] = (False, False, True, True),n_blocks: int = 2):
+        super().__init__()
+        n_resolutions = len(ch_mults)
+        self.image_proj = nn.Conv2d(image_channels, n_channels, kernel_size=(3, 3), padding=(1, 1))  #Project image into feature map 
+        self.time_emb = TimeEmbedding(n_channels * 4)
+        down = []
+        out_channels = in_channels = n_channels
+        for i in range(n_resolutions):
+            out_channels = in_channels * ch_mults[i]
+            for j in range(n_blocks):
+                down.append(DownBlock(in_channels, out_channels, n_channels*4, is_attn[i]))
+                in_channels = out_channels
+            if i < n_resolutions - 1:
+                down.append(Downsample(in_channels))
+        self.down = nn.ModuleList(down)
+        self.middle = MiddleBlock(out_channels, n_channels * 4)
+        up = []
+        in_channels = out_channels
+        for i in reversed(range(n_resolutions)):
+            out_channels = in_channels
+            for j in range(n_blocks):
+                up.append(UpBlock(in_channels, out_channels, n_channels*4, is_attn[i]))
+            out_channels = in_channels // ch_mults[i]
+            up.append(UpBlock(in_channels, out_channels, n_channels*4, is_attn[i]))
+            in_channels = out_channels
+            if i > 0:
+                up.append(Upsample(in_channels))
+        self.up = nn.ModuleList(up)
+        self.norm = nn.GroupNorm(8, n_channels)
+        self.act = Swish()
+        self.final = nn.Conv2d(in_channels, image_channels, kernel_size=(3, 3), padding=(1, 1))
+    
+    def forward(self, x: torch.Tensor, t: torch.Tensor) :
+        t = self.time_emb(t)
+        x = self.image_proj(x)
+        h = [x]
+        
+        for m in self.down:        #First half of U-Net 
+            x = m(x,t)
+            h.append(x)
+            
+        x = self.middle(x, t)       #bottom of the UNet
+        
+        for m in self.up:            #Second half of U-Net 
+            if isinstance(m, Upsample):
+                x = m(x, t)
+            else:               #Get the skip connection from first half of U-Net and concatenate 
+                s = h.pop()
+                x = torch.cat((x, s), dim=1)
+                x = m(x, t)
+        return self.final(self.act(self.norm(x)))  #final normalization and activation before output  
+        
+        
